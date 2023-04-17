aedbb0e0e299389bc7414332f5c10b37e5744487
fallinLeo <122957896+fallinLeo@users.noreply.github.com>
2023-04-17 17:46:43 +0900

    04/17
    Circles ->homework

---
+++ 11_CP_Circles_폼만/Circles/Circle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+namespace Circles
+{
+
+    class Circle
+    {
+        private static Random rnd = new Random();
+        private double xcen, ycen, radius;
+        private int red, green, blue;
+        private double xmov, ymov;
+        public bool rev = false;
+
+        // 생성자 - 주어진 위치에 랜덤 크기, 랜덤 색의 원 생성
+        public Circle(double xcen, double ycen)
+        {
+            this.xcen = xcen;
+            this.ycen = ycen;
+            this.radius = rnd.Next(10) + 2;
+            this.red = rnd.Next(200);
+            this.green = rnd.Next(200);
+            this.blue = rnd.Next(200);  // 너무밝지않게
+            while (true) { this.xmov = rnd.Next(-3, 3); if (xmov != 0) break; }
+            while (true) { this.ymov = rnd.Next(-3, 3); if (ymov != 0) break; }
+        }
+
+        // 움직임
+        public void move(double xmin, double xmax, double ymin, double ymax)
+        {
+            if (xcen <= xmin + radius) xmov = Math.Abs(xmov);
+            if (xcen >= xmax - radius) xmov = -Math.Abs(xmov);
+
+            if (ycen <= ymin + radius) ymov = Math.Abs(ymov);
+            if (ycen >= ymax - radius) ymov = -Math.Abs(ymov);
+
+            xcen += xmov;
+            ycen += ymov;
+        }
+
+        // 그리기
+        public void draw(TCanvas canvas)
+        {
+            canvas.DrawEllipse(Color.FromArgb(this.red, this.green, this.blue),
+            this.xcen, this.ycen, this.radius * 2, this.radius * 2);
+        }
+
+        // 주어진 점이 내부인지 판단
+        public bool isinside(double xp, double yp)
+        {
+            double dx = xp - this.xcen;
+            double dy = yp - this.ycen;
+            double dist = Math.Sqrt(dx * dx + dy * dy);
+            return (dist <= this.radius) ? true : false;
+        }
+        //내부 원 지우기
+        public void innercheck(Circle c2)
+        {
+            if (this.xcen + this.radius >= c2.xcen + c2.radius && this.xcen - this.radius <= c2.xcen - c2.radius)
+            {
+                if (this.ycen + this.radius >= c2.ycen + c2.radius && this.ycen - this.radius <= c2.ycen - c2.radius) c2.rev = true;
+
+            }
+            else if (this.xcen + this.radius <= c2.xcen + c2.radius && this.xcen - this.radius >= c2.xcen - c2.radius)
+            {
+                if (this.ycen + this.radius <= c2.ycen + c2.radius && this.ycen - this.radius >= c2.ycen - c2.radius) this.rev = true;
+
+            }
+        }
+    }
+}
